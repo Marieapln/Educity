2b7ed6dc70f86a007248cc56154f193ac9154a8b
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-06-09 12:47:50 +0100

    create partials for category, subcategory, and platform form components, with appropriate styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,19 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  before_action :set_platforms, only: %i[home]
+  before_action :set_categories, only: %i[home]
 
-  def home
+  def home; end
+
+  private
+
+  def set_platforms
+    @platforms = []
+    Course.all.uniq(&:platform).each { |course| @platforms << course.platform.capitalize }
+  end
+
+  def set_categories
+    @categories = []
+    Course.all.uniq(&:category).each { |course| @categories << course.category.split.each(&:capitalize).join(' ') }
   end
 end
