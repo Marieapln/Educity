4f2bdd052c51205ab5b5f4857a41f8590c553102
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-06-14 00:03:58 +0100

    added search bar to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,8 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
 
 const buildMap = (mapElement) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -8,11 +11,28 @@ const buildMap = (mapElement) => {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
   };
+
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb' // <-- use your own!
+  });
   
   const addMarkersToMap = (map, markers) => {
     markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+    
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+    
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(element)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(map);
     });
   };
@@ -28,9 +48,15 @@ const buildMap = (mapElement) => {
     if (mapElement) {
       const map = buildMap(mapElement);
       const markers = JSON.parse(mapElement.dataset.markers);
+      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,mapboxgl: mapboxgl }));
       addMarkersToMap(map, markers);
       fitMapToMarkers(map, markers);
+      
     }
   };
 
+
+    
+  
 export { initMapbox };
+
