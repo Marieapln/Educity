fc693079c5a9b3c62328984d9420ed5be13992da
Marie Apolozan <marie.apolozan@gmail.com>
2021-06-14 20:13:09 +0300

    commit

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -17,18 +17,25 @@ class CoursesController < ApplicationController
     @url = params[:url]
     html_file = URI.open(@url).read
     html_doc = Nokogiri::HTML(html_file)
-        # binding.pry
+        binding.pry
 
     platform =  html_doc.search('meta[property="og:site_name"]')[0].attributes["content"].value
     if platform == "Codecademy"
-      @title = html_doc.search('h1.heading__2X5Zo7G1JUo6H9EbL5j-mP').text.strip
+      title = html_doc.search('h1.heading__2X5Zo7G1JUo6H9EbL5j-mP').text.strip
+      hours = html_doc.search('.gamut-1sk71u6-Text')[2].children.text.strip.match(/\d/)[0].to_i
+      Course.new(title: title, platform:platform, url: @url, course_length: hours).save
+
+
     elsif platform == "Coursera"
-      @title = html_doc.search('h1.banner-title').text.strip
+      title = html_doc.search('h1.banner-title').text.strip
       subcategories = []
       html_doc.search('._rsc0bd').each {|c| subcategories << c.attributes["title"].value}
-    end
+      category = html_doc.search('.breadcrumb-item').text.strip
+      months = html_doc.search('._1tu07i3a')[4].children[0].children.children.text.match(/\d/)[0].to_i
+      hours = html_doc.search('._1tu07i3a')[4].children[1].children.children.text.match(/\d/)[0].to_i
+      Course.new(title: title, url: @url, platform: platform, subcategories: subcategories, course_length: months*hours).save
 
-    Course.new(title: @title, url: @url, platform: platform, subcategories: subcategories).save
+    end
   end
 
 
