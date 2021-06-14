796ad3374b52921005286d95abf7e3cb0d7fd5ce
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-06-14 17:01:00 +0100

    recover all today's code finally?

--- db/migrate/20210614151746_add_city_to_teams.rb
+++
@@ -1,7 +0,0 @@
-class AddCityToTeams < ActiveRecord::Migration[6.0]
-  def change
-    add_column :teams, :city, :string
-    add_column :teams, :latitude, :float
-    add_column :teams, :longitude, :float
-  end
-end
