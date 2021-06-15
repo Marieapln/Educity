7aeb8a3bfd8a11258c08552b881a789e6367644d
Marie Apolozan <marie.apolozan@gmail.com>
2021-06-15 15:14:32 +0300

    added images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.where.not(latitude: nil, longitude: nil)
 
-      @markers = @users.geocoded.map do |user| 
-        {
-          lat: user.latitude,
-          lng: user.longitude,
-          info_window: render_to_string(partial: "info_window", locals: { user: user })
-         }
-      end
-  end
+  # def index
+  #   @users = User.where.not(latitude: nil, longitude: nil)
+
+  #     @markers = @users.geocoded.map do |user|
+  #       {
+  #         lat: user.latitude,
+  #         lng: user.longitude,
+  #         info_window: render_to_string(partial: "info_window", locals: { user: user })
+  #        }
+  #     end
+  # end
+
+
+
+
 end
