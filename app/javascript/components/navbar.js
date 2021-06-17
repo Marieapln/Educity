6399896e95272b252d53acc2cb5fb4c4f3548a41
Marie Apolozan <marie.apolozan@gmail.com>
2021-06-17 16:30:50 +0300

    commit

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 170) {
+      if (window.scrollY >= 120) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
